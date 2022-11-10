4cac337fe32ad7a949660b3476136ece348d8619
hunjeong93 <rhkrgnswjd@naver.com>
2022-11-10 11:11:01 +0900

    generic exam

--- src/section1/GenericEx.java
+++ src/section1/GenericEx.java
@@ -2,9 +2,12 @@ package section1;
 
 public class GenericEx {
     public static void main(String[] args) {
-        Box<Toy> toyBox = new Box<>();
-        toyBox.setItem(new miniCar()); // 다형성 적용
+//        Box<Toy> toyBox = new Box<>();
+//        toyBox.setItem(new miniCar()); // 다형성 적용
 //        toyBox.setItem(new superCar()); // 에러
+
+        Box<miniCar> toyBox = new Box<>();
+//        Box<superCar> superCarBox = new Box<>(); //에러
     }
 }
 
@@ -13,7 +16,7 @@ class Toy {}
 class miniCar extends Toy { }
 class superCar {}
 
-class Box<T>{
+class Box<T extends Toy>{ // Box클래스를 인스턴스화 할 때 타입으로 Toy 클래스의 하위 클래스만 지정하도록 제한한다.
     private T item;
 
     public T getItem() {
