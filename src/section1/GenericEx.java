8f2a11888be565b380e7109ec94a30a01e883f8f
hunjeong93 <rhkrgnswjd@naver.com>
2022-11-10 11:18:23 +0900

    generic exam

--- src/section1/GenericEx.java
+++ src/section1/GenericEx.java
@@ -6,17 +6,22 @@ public class GenericEx {
 //        toyBox.setItem(new miniCar()); // 다형성 적용
 //        toyBox.setItem(new superCar()); // 에러
 
-        Box<miniCar> toyBox = new Box<>();
+//        Box<miniCar> toyBox = new Box<>();
 //        Box<superCar> superCarBox = new Box<>(); //에러
+
+        //인스턴스화
+        Box<Toy> toyBox = new Box<>();
+        Box<miniCar> miniCarBox = new Box<>();
     }
 }
+interface Plastic{}
+class Toy implements Plastic{}
 
-class Toy {}
-
-class miniCar extends Toy { }
+class miniCar extends Toy implements Plastic{ }
 class superCar {}
 
-class Box<T extends Toy>{ // Box클래스를 인스턴스화 할 때 타입으로 Toy 클래스의 하위 클래스만 지정하도록 제한한다.
+//class Box<T extends Plastic>{ // Box클래스를 인스턴스화 할 때 타입으로 Toy 클래스의 하위 클래스만 지정하도록 제한한다.
+    class Box<T extends Toy & Plastic>{ //특정 클래스를 상속받으면서 동시에 특정 인터페이스를 구현한 클래스만 타입으로 지정할 수 있도록 제한 클래스 먼저 위치
     private T item;
 
     public T getItem() {
